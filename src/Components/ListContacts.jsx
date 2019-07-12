5bc1630932bad0d3b85cd2d8e7e851c2da3b9a39
sudhish nair <sudhishanir53@gmail.com>
2019-07-12 16:49:21 +0530

    :sparkles: Implemented contact serach and sort

--- src/Components/ListContacts.jsx
+++ src/Components/ListContacts.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import * as firebase from "firebase";
+import escapeRegExp from "escape-string-regexp";
+import sortBy from "sort-by";
 
 class ListContacts extends Component {
   constructor() {
@@ -20,7 +22,8 @@ class ListContacts extends Component {
     // console.log(dataitems)
     this.setState({
       ...this.state,
-      entireData: dataitems
+      entireData: dataitems,
+      query: ""
     });
   };
   // Callback method called when receives some error during the retrieval of data from the database
@@ -29,37 +32,67 @@ class ListContacts extends Component {
   };
   // Method handles the contact delete
   handleRemoveContact = e => {
-    console.log(e); 
-    firebase.database().ref("ContactsList/"+e).remove();
-    console.log("item removed")
+    console.log(e);
+    firebase
+      .database()
+      .ref("ContactsList/" + e)
+      .remove();
+    console.log("item removed");
+  };
+  //Method to set the state of the query/search field
+  handleQueryChange = query => {
+    this.setState({
+      query: query.trim()
+    });
   };
   //@Todo : Check the image url its not working currently
-  
+
   render() {
+    let showingContacts;
+    if (this.state.query) {
+      const match = new RegExp(escapeRegExp(this.state.query), "i");
+      showingContacts = this.state.entireData.filter(contact =>
+        match.test(contact.name)
+      );
+    } else {
+      showingContacts = this.state.entireData;
+    }
+    showingContacts.sort(sortBy('name'))
     return (
-      <ul className="contact-list">
-        {this.state.entireData.map((eachcontent, index) => (
-          <li key={index} className="contact-list-item">
-            <div
-              className="contact-avatar"
-              style={{
-                backgroundImage: `url(${eachcontent.imgurl})`
-              }}
-            />
-            {console.log(eachcontent.imgurl)}
-            <div className="contact-details">
-              <p>{eachcontent.name}</p>
-              <p>{eachcontent.email}</p>
-            </div>
-            <button
-              className="contact-remove"
-              onClick={e => this.handleRemoveContact(index)}
-            >
-              Remove
-            </button>
-          </li>
-        ))}
-      </ul>
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <input
+            className="search-contacts"
+            type="text"
+            placeholder="search contact's here"
+            value={this.state.query}
+            onChange={e => this.handleQueryChange(e.target.value)}
+          />
+        </div>
+        <ul className="contact-list">
+          {showingContacts.map((eachcontent, index) => (
+            <li key={index} className="contact-list-item">
+              <div
+                className="contact-avatar"
+                style={{
+                  backgroundImage: `url(${eachcontent.imgurl})`
+                }}
+              />
+              {console.log(eachcontent.imgurl)}
+              <div className="contact-details">
+                <p>{eachcontent.name}</p>
+                <p>{eachcontent.email}</p>
+              </div>
+              <button
+                className="contact-remove"
+                onClick={e => this.handleRemoveContact(index)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
